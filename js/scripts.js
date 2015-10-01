d234e3121af4c3b9b0f8adbc38028ef6885653e7
Arden Thira & Mike McAllister <student@epicodus.com>
2015-09-30 18:47:04 -0700

    add new page elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
+$(function)( {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS AWESOME!!");
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
-});
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+})
